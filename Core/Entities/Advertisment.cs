ffc68360f23026630c117d482837acd8a97b7fe6
SerhiiHerasymiuk <@DESKTOP-8DFR7S5>
2023-07-03 23:42:36 +0300

    Added Advertisment info

--- Core/Entities/Advertisment.cs
+++ Core/Entities/Advertisment.cs
@@ -5,8 +5,13 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Location { get; set; }
+        public string ContactPerson { get; set; }
+        public string ContactPhoneNumber { get; set; }
         public List<AdvertismentImage> AdvertismentImages { get; set; }
         public int SubcategoryId { get; set; }
         public Subcategory Subcategory { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
     }
 }
---
+++ Infrastructure/Migrations/20230703204203_AddAdvertismentInfo.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdvertismentInfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContactPerson",
+                table: "Advertisments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactPhoneNumber",
+                table: "Advertisments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Advertisments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Advertisments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId1",
+                table: "Advertisments",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Advertisments_UserId1",
+                table: "Advertisments",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Advertisments_AspNetUsers_UserId1",
+                table: "Advertisments",
+                column: "UserId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Advertisments_AspNetUsers_UserId1",
+                table: "Advertisments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Advertisments_UserId1",
+                table: "Advertisments");
+
+            migrationBuilder.DropColumn(
+                name: "ContactPerson",
+                table: "Advertisments");
+
+            migrationBuilder.DropColumn(
+                name: "ContactPhoneNumber",
+                table: "Advertisments");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Advertisments");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Advertisments");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Advertisments");
+        }
+    }
+}
