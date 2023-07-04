7a6d0016329fc2bbc3006b88d4967c05dd75e97f
SerhiiHerasymiuk <@DESKTOP-8DFR7S5>
2023-07-05 00:15:59 +0300

    Added SubcategoryService

---
+++ Core/Specifications/Subcategories.cs
@@ -0,0 +1,33 @@
+﻿using Ardalis.Specification;
+using Core.Entities;
+
+namespace Core.Specifications
+{
+    public static class Subcategories
+    {
+        public class ById : Specification<Subcategory>
+        {
+            public ById(int id)
+            {
+                Query
+                    .Where(x => x.Id == id);
+            }
+        }
+        public class All : Specification<Subcategory>
+        {
+            public All()
+            {
+                Query
+                    .Include(x => x);
+            }
+        }
+        public class ByCategoryId : Specification<Subcategory>
+        {
+            public ByCategoryId(int categoryId)
+            {
+                Query
+                    .Where(c => c.CategoryId == categoryId);
+            }
+        }
+    }
+}
