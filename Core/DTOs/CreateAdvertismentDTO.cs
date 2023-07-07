b8a80eab9003ee39974055779d20e99f09d037a4
SerhiiHerasymiuk <@DESKTOP-8DFR7S5>
2023-07-07 22:54:01 +0300

    Added Advertisment Price

--- Core/DTOs/CreateAdvertismentDTO.cs
+++ Core/DTOs/CreateAdvertismentDTO.cs
@@ -14,6 +14,7 @@ namespace Core.DTOs
         public string Location { get; set; }
         public string ContactPerson { get; set; }
         public string ContactPhoneNumber { get; set; }
+        public decimal Price { get; set; }
         public List<IFormFile>? AdvertismentImages { get; set; }
         public int SubcategoryId { get; set; }
         public string UserId { get; set; }
